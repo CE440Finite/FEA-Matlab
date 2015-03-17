2a1a19572a8de4b1333b39287bc9f9a1a3ff876a
Ricky Bhola <ricky.b.bhola@gmail.com>
2015-03-16 23:08:40 -0400

    started master loop for matrix

--- coordinateNum.m
+++ coordinateNum.m
@@ -1,1 +1,18 @@
-%% Joint Coordinate Numbers, Ricky
+%% Joint Coordinate Numbers, Sec 4.2
+
+nsc = zeros(1,ncjt*nj);  %dimension and allocate coordinate number vector
+i = 1; j = 0; k = ndof;
+
+while i <= nj   % main loop
+    icount = 0;
+    i1 = 1;
+    while i1 <= ns
+        if msup(i1,1) ==1
+            icount = 1;
+            i2 = 1;
+        else
+            i1 = i1 + 1;
+            break
+        end
+        % end for now
+end
