6328b3da9b94e1784254f9e64a6efdf06e808169
Ricky Bhola <ricky.b.bhola@gmail.com>
2015-03-22 23:00:34 -0400

    Added additional loops
    Added loops to : check joint has restraint in msup(i,1) and check if
    condition exist in msup(i,2:3)

--- coordinateNum.m
+++ coordinateNum.m
@@ -6,12 +6,29 @@ i = 1; j = 0; k = ndof;
 while i <= nj   % main loop
     icount = 0;
     i1 = 1;
-    while i1 <= ns
-        if msup(i1,1) ==1
+    while i1 <= ns %check each joint, starting at 1
+        if msup(i1,1) ==1   %check that joint is restrained
             icount = 1;
             i2 = 1;
         else
             i1 = i1 + 1;
             break
         end
+        if i2 < ncjt
+            i3 = (i-1)*ncjt + i2;
+        else
+            break
+        end
+        if msup(i1, i2 + 1) == 1 %check that there is a restraint, cond = 1.
+            k = k + 1;
+            nsc(i3) = k;
+            i2 = i2 + 1
+        else
+            j = j + 1;
+            nsc(i3) = j;
+        end
+    end
+            
+            
+            
 end
