c917ee9673468ec374ced98a75781c397747ff62
Ricky Bhola <ricky.b.bhola@gmail.com>
2015-04-04 18:39:12 -0400

    update 2 to output script
    Added more sections.

--- report_output.m
+++ report_output.m
@@ -11,10 +11,14 @@ fprintf(fid,'Date & Time: %s\n\n',datestr(now));
 fprintf(fid,'\n\n');
 
 fprintf(fid,'** Program Input **\n\n');
+
 fprintf(fid,'Number of Joints: %u\n',nj);
 fprintf(fid,'Number of Members: %u\n',length(mprp));
 fprintf(fid,'Number of Defined Modulus of Elasticity : %u\n',length(em));
 fprintf(fid,'Number of Defined Cross Sections: %u\n\n',length(cp));
+
+fprintf(fid,'Joint Coordinates\n');
+fprintf(fid,'=================\n');
 
 fprintf(fid,'Joint      X-Coordinate       Y-Coordinate\n');
 fprintf(fid,'_____      ____________       ____________\n\n');
@@ -23,6 +27,9 @@ for n = 1:length(coord)
 end
 fprintf(fid,'\n');
 
+fprintf(fid,'Joint Restraint\n');
+fprintf(fid,'=================\n');
+
 fprintf(fid,'Joint      X-Restraint       Y-Restraint\n');
 fprintf(fid,'_____      ___________       ___________\n');
 for n = 1:length(msup)
@@ -30,7 +37,43 @@ for n = 1:length(msup)
 end  
 fprintf(fid,'\n');
 
-fprintf(fid,'1 = restrained, 0 = not restrained\n');
+fprintf(fid,'1 = restrained, 0 = not restrained\n\n');
+
+fprintf(fid,'Defined Modulus of Elasticity\n');
+fprintf(fid,'=============================\n\n');
+fprintf(fid,'No.      Modulus of Elasticity\n');
+fprintf(fid,'__       _____________________\n\n');
+for n = 1:length(em)
+    fprintf(fid,'%u             %.3e\n',n,msup(n));
+end
+fprintf(fid,'\n');
+fprintf(fid,'Defined Cross Sectional Area\n');
+fprintf(fid,'============================\n\n');
+fprintf(fid,'No.      Cross Sectional Area\n');
+fprintf(fid,'__       ____________________\n\n');
+for n = 1:length(cp)
+    fprintf(fid,'%u             %.3e\n',n,cp(n));
+end
+fprintf(fid,'\n');
+
+fprintf(fid,'Member Data\n');
+fprintf(fid,'===========\n\n');
+fprintf(fid,'Member No.     Start Joint No.     End Joint No.   Cross Section   Modulus of Elasticity\n');
+fprintf(fid,'_________      _______________     ____________    _____________   _____________________\n\n');
+for n = 1:length(mprp)
+    fprintf(fid,'%3u                    %u                 %u          %.3e           %.3e\n',... 
+        n,mprp(n,1),mprp(n,2),mprp(n,4),mprp(n,3));
+end
+fprintf(fid,'\n');
+fprintf(fid,'Joint Loads\n');
+fprintf(fid,'=============================\n\n');
+fprintf(fid,'Joint No.      X-Load      Y-Load\n');
+fprintf(fid,'________       ______      ______\n\n');
+
+for n = 1:length(jp)
+    fprintf(fid,'%3u        %.2e        %.2e\n',jp(n),pj(n,1),pj(n,2));
+end
+
 
 
 
