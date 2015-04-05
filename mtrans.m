ff761086a638d37c56d62236d387fc848510d355
Ricky Bhola <ricky.b.bhola@gmail.com>
2015-04-05 16:12:12 -0400

    Created MTRANS Subroutine

---
+++ mtrans.m
@@ -0,0 +1,21 @@
+function t = mtrans(cx,cy,ncjt)
+i = 1;
+while i <= 2*ncjt
+    j = 1;
+    while j <= 2*ncjt
+        t(i,j) = 0;
+        j = j + 1;
+    end
+    i = i + 1;
+end
+t(1,1) = cx;
+t(1,2) = cy;
+t(3,3) = cx;
+t(3,4) = cy;
+t(2,1) = -cy;
+t(2,2) = cx;
+t(4,3) = -cy;
+t(4,4) = cx;
+
+disp(t)
+end
