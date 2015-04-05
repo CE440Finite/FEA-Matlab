84e9fb7b89f0edee8f83f368af2de4e4b46832a7
Ricky Bhola <ricky.b.bhola@gmail.com>
2015-04-04 22:31:05 -0400

    Added 'name' to project title.

--- report_output.m
+++ report_output.m
@@ -4,7 +4,7 @@
 fid = fopen('report.txt','w'); %create new file to write data to.
 
 
-fprintf(fid,'Project Title: %s\n','Problem 1');
+fprintf(fid,'Project Title: %s\n',name);
 fprintf(fid,'User Name: %s\n',getenv('USER'));
 fprintf(fid,'Operating System: %s\n',getenv('ARCH'));
 fprintf(fid,'Date & Time: %s\n\n',datestr(now));
