646ac04cc365df284bdfc881667698b8448bb42b
Ricky Bhola <ricky.b.bhola@gmail.com>
2015-04-05 16:19:48 -0400

    Created MDISPL Subroutine

---
+++ mdispl.m
@@ -0,0 +1,19 @@
+function mdispl(ncjt, v, t)
+i = 1;
+while i <= 2*ncjt
+    u(i) = 0;
+    i = i + 1;
+end
+
+i = 1;
+
+while i <= 2*ncjt
+    j = 1;
+    while j <= 2*ncjt
+        u(i) = u(i) + t(i,j) * v(j);
+        j = j + 1;
+    end
+    i = i + 1;
+end
+disp(u)
+end
