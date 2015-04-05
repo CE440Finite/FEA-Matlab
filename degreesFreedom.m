96d35a15ff0a5e739937c06f2872429aafb96288
Ricky Bhola <ricky.b.bhola@gmail.com>
2015-04-05 14:57:28 -0400

    Finished Script
    Added change  while condition, ncjt + 1 instead of 3

--- degreesFreedom.m
+++ degreesFreedom.m
@@ -4,7 +4,7 @@ i = 1; nr = 0;ncjt = 2;		%set the number of restraints (nr) = 0, for trusses ncj
 		
 while i <= ns		%msup matrix has dimensions (ns,3) where ns = number of supported joints, iterations occurs vertically
     i1 = 2;		%iteration excludes first row of msup and works on msup(:,2:3)
-    while i1 <= 3	%check columns 2 and 3, iteration occurs horizontally
+    while i1 <= ncjt + 1	%check columns 2 and 3, iteration occurs horizontally
         if msup(i,i1) == 1	%check that he degree of freedom is restrained
             nr = nr + 1;		%increase nr by one
             i1 = i1 + 1;		%increase i1 to check next column
