83347e6e9fee450e9405ccbdd1d4e73759dee461
Ricky Bhola <ricky.b.bhola@gmail.com>
2015-04-05 19:58:28 -0400

    Changed the counters to match the book's flow chart and lower case variables.

--- JointDisplacement.m
+++ JointDisplacement.m
@@ -1,26 +1,28 @@
-Count1 = 1;
-while Count1 <= NDOF
-	Z1 = S(Count1,Count1);
-	Count2 = 1;
-	while Count2 <= NDOF
-		S(Count1,Count2) = S(Count1,Count2)/Z1;
-		Count2 =Count2+1;
-	end
-	P(Count1) = P(Count1)/Z1;
-	Count3 = 1;
-	while Count3 <=NDOF
-		if Count3 == Count1
-			Count3 = Count3 + 1;
-		else
-			Z = S(Count3, Count 1);
-			Count4 = 1;
-			while Count4<= NDOF
-				S(Count3,Count4) = S(Count3,Count4) - S(Count1,Count4)*Z;
-				Count4 = Count4 +=1;
-			end
-			P(Count3) = P(Count3) - P(Count1)*Z;
-			Count3 = Count3 + 1;
-		end
-	end
-	Count1 = Count1 = Count 1+1;
+% Calculation of joint displacements by Gauss- Jordan Elimination.
+
+i = 1;
+while i <= ndof
+    z1 = s(i,i);
+    j = 1;
+    while j <= ndof
+        s(i,j) = s(i,j)/Z1;
+        j =j+1;
+    end
+    p(i) = p(i)/z1;
+    k = 1;
+    while k <= ndof
+        if k == i
+            k = k + 1;
+        else
+            z = s(k, i);
+            m = 1;
+            while m <= ndof
+                s(k,m) = s(k,m) - s(i,m)*z;
+                m = m + 1;
+            end
+            p(k) = p(k) - p(i)*z;
+            k = k + 1;
+        end
+    end
+    i = i + 1;
 end
