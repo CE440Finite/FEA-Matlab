1ae06a147fb500fdf248ae112126b532bd857794
Ricky Bhola <ricky.b.bhola@gmail.com>
2015-04-17 17:26:07 -0400

    Output Script

--- report_output.m
+++ report_output.m
@@ -1,6 +1,7 @@
 % This script will write the program input and output to a text file called
 % 'report_projectName.txt' 
 
+% -- INPUT
 fname = strcat('report_',name,'.txt');
 fid = fopen(fname,'w'); %create new file to write data to.
 product = ver('matlab');
@@ -84,4 +85,48 @@ for n = 1:length(jp)
 end
 fprintf(fid,'\n\n');
 
+% -- OUTPUT
+stressC = cell(nm,1);
+for i = 1:nm
+    if sign(aforce(i))== -1
+        stressC{i} = {'Tension'};
+    else
+        stressC{i} = {'Compression'};
+    end
+end
+fprintf(fid,'-------------\n');
+fprintf(fid,'Program Output\n');
+fprintf(fid,'-------------\n\n');
+
+fprintf(fid,'Member Axial Force\n');
+fprintf(fid,'==================\n\n');
+
+fprintf(fid,'Member No.      Axial Force        Compression/Tension\n');
+fprintf(fid,'_________       ___________        ___________________\n\n');
+for i = 1:nm
+    fprintf(fid,'%4u              %5.2e         %s\n', i, aforce(i),stressC{i}{1});
+end
+fprintf(fid,'\n\n');
+
 fclose(fid);
+
+%% STRUCTURE PLOT
+% INITIAL SHAPE
+for n=1:length(mprp)
+    str = num2str(n);
+    startjoint = mtab(n,1);
+    sjoint = coord(startjoint,:);
+    endjoint = mtab(n,2);
+    ejoint = coord(endjoint,:);
+    
+    crds = vertcat(sjoint,ejoint);
+    plot(crds(:,1),crds(:,2),'k-');
+    hold on
+end
+j = 1;
+for i = jp'
+    quiver(coord(i,1),coord(i,2),pj(j,1),pj(j,2),0.3, 'Color','red','LineWidth', ...
+        2, 'MaxHeadSize',.7)
+    hold on
+    j = j + 1;
+end
