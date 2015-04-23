e81a427b8e4397971b0e08bd834367307f8a9dd7
Ricky Bhola <ricky.b.bhola@gmail.com>
2015-04-23 12:05:16 -0400

    ADDED DISPLACEMENT OUTPUT VECTOR

--- trussAnalysis.m
+++ trussAnalysis.m
@@ -8,7 +8,7 @@ name = input('Enter a project name: ','s');
 disp(' ')
 
 % Ask for joint coordinates.
-disp('Input must be enclosed in the bracket [] with each entry separated by a space.')
+disp('Input must be enclosed in the bracket [] with each entry separated by a space or comma.')
 disp('Consistent units are required as this program will not convert units.')
 disp('  ')
 
@@ -208,7 +208,7 @@ while im <= nm
     % -- MDISPG
     i = 1;
     while i <= 2*ncjt
-        v(i) = 0
+        v(i) = 0;
         i = i + 1;
     end
     j = (jb - 1)* ncjt;
@@ -231,7 +231,7 @@ while im <= nm
         j = j + 1;
         n = nsc(j);
         if n <= ndof
-            v(i) = p(n);
+            v(i) = p(n)
             i = i + 1;
         else
             i = i + 1;
@@ -351,6 +351,42 @@ while im <= nm
     
     im = im + 1;
 end
+%%
+dispvecloc = 1;  %place in displacement vector
+xtrans = zeros(nj,1); % stores translations for joint
+ytrans = zeros(nj,1);  % stores translations for joint
+xsupchk = 1; % variable needed for cheking restraint conditions later in loop
+ysupchk = 1; % variable needed for cheking restraint conditions later in loop
+for n = 1:nj %checks all joints
+	exist = 0; % variable for determing is a joint is restrained 
+	for k = 1:ns %checks against all restrained joint
+		if n == msup(k) %if a joint is restrained
+			exist =  exist +1; %indicate it is restrained
+			xsupchk = msup(k,2); %restrain conditions of x for joint
+			ysupchk = msup(k,3); %restrain conditions of y for joint
+		else 
+			exist = exist; %for loop does not terminate,need to insure conditionstay
+		end
+	end
+	if exist >= 1 % if joint is restrained
+		if xsupchk == 0; %if x is free
+			xtrans(n) = p(dispvecloc); % then x is displaced by 
+			dispvecloc = dispvecloc + 1; %change location is P vector for next loop
+		end
+		if ysupchk == 0; %if y is free
+			ytrans(n) = p(dispvecloc); % then y is displaced by 
+			dispvecloc = dispvecloc + 1; %change location is P vector for next loop
+		end
+	else % if joint is not restrained 
+		xtrans(n) = p(dispvecloc); % x displace 
+		dispvecloc = dispvecloc + 1; % shift over in P
+		ytrans(n) = p(dispvecloc); % Y displace
+		dispvecloc = dispvecloc + 1; % shift over in P for next loop
+	end
+end
+
+% should note that this onlt works if the joints are put in in order i.e restrained joints are put in, in increasing values. 
+
 
 
 
